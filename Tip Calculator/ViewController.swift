640d74503b6d95bee2ee7265080b41d718b6ea0f
Sherry Liu <2506-sl3689@users.noreply.gitlab.cci.drexel.edu>
2022-08-20 10:59:59 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Sherry Liu on 8/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
